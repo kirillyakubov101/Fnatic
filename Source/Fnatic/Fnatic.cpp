342fa3e859946367c5f37e3bcbef428937a69f7f
kirillyakubov101 <59371020+kirillyakubov101@users.noreply.github.com>
2023-04-10 11:32:25 -0400

    Init Commit

---
+++ Source/Fnatic/Fnatic.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Fnatic.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Fnatic, "Fnatic" );
---
+++ Source/Fnatic/FnaticGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FnaticGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FNATIC_API AFnaticGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
